c10ee4925a0128bc389b632b21f603790f6953c4
Micah J. Murray <micah.murray@gmail.com>
2025-06-27 20:11:01 -0400

    Updated db.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -10,7 +10,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port:3306
+  port:3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
